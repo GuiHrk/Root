227107a38d65e4c65c8a61944aae02375af83931
Guilherme Henrique <guihenrique17020@uin9.edu.br>
2025-09-13 20:09:56 -0300

    Conexão ao banco de dados online

--- backend-nodes/index.js
+++ backend-nodes/index.js
@@ -3,21 +3,46 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const { Sequelize } = require("sequelize");
+
+console.log("Variáveis carregadas:", {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  pass: process.env.DB_PASSWORD ? "***" : "VAZIA",
+  db: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port: process.env.DB_PORT,
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => console.log("✅ Conectado ao MySQL Railway"))
+  .catch((err) => console.error("❌ Falha ao conectar", err));
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { 
+  cors: {
     origin: "http://127.0.0.1:5500",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("O SERVIDOR ESTÁ ONLINE ");
+  res.send("O SERVIDOR ESTÁ ONLINE ");
 });
 
 const PORT = process.env.PORT || 3000;
