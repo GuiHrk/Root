879cec018d69a31a3a907fe58d0e172322df9151
GuiHrk <guihenrique17020@gmail.com>
2025-10-26 20:19:52 -0300

    Adicionando Funcionalidade de login

---
+++ Login-Folder/Login.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+  
+    form.addEventListener("submit", async (e) => {
+      e.preventDefault();
+  
+      const email = document.getElementById("email").value.trim();
+      const senha = document.getElementById("senha").value.trim();
+  
+      if (!email || !senha) {
+        alert("Preencha todos os campos!");
+        return;
+      }
+  
+      try{
+        const response = await fetch("https://root-backend-chat.onrender.com", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, senha }),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        alert(data.error || "Falha no login. Verifique suas credenciais.");
+        return;
+      }
+
+      localStorage.setItem("userId", data.user.id);
+      localStorage.setItem("userName", data.user.nome);
+
+      alert(`Bem-vindo, ${data.user.nome}! Redirecionando...`);
+
+      setTimeout(() => { 
+        window.location.href = "/Root/chat.html";
+      }, 1500);
+
+    } catch (error) {
+        console.error("Erro durante o login:", error);
+        alert("Erro ao conectar com o servidor.");
+      }
+    });
+  });
